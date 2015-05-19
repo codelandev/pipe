3dbd5c46371765cc5e185ada7bc845c24fb802ab
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2015-05-19 13:25:23 -0300

    Precompile just one JS and one CSS files in production
    main.js and style.css can import others JS and CSS files

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/asset_pipeline'
 
 class App < Sinatra::Base
-  set :assets_precompile, %w(*.js styles.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+  set :assets_precompile, %w(main.js style.css *.png *.jpg *.svg *.eot *.ttf *.woff)
   set :assets_css_compressor, :sass
   set :assets_js_compressor, :uglifier
   register Sinatra::AssetPipeline
